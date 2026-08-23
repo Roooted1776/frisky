4ad46801e113994169b76fa2d1321c0e1505c0e6
M <maxaguilaraasted@gmail.com>
2026-08-23 00:11:40 -0700

    Bump root SW cache to v117

--- sw.js
+++ sw.js
@@ -11,7 +11,7 @@
  * putShell is HTML-only. Optional assets use putAsset so logos / sw.js never
  * overwrite shell keys (that poison served PNG/JS as /tapper/).
  */
-var CACHE = 'redmed-tapper-v116';
+var CACHE = 'redmed-tapper-v117';
 var ASSETS = [
   './pheart.png',
   './BrandLogo.png',
